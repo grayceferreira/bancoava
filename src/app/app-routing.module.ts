12093150dd79d1b8721924ef6c29b88f3538bcac
kruschewskyfe@hotmail.com <kruschewskyfe@hotmail.com>
2020-02-08 10:11:20 -0300

    criação de rotas e componentes compatilhados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,34 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { NotFoundComponent } from './components/shared/not-found/not-found.component';
+import { HomeComponent } from './pages/home/home.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { StatementsComponent } from './pages/statements/statements.component';
+import { TransfersComponent } from './pages/transfers/transfers.component';
 
 
 const routes: Routes = [{
   path: 'login',
   component: LoginPageComponent
-},{
-  path: '**',
+}, {
+  path: 'home',
+  component: HomeComponent
+}, {
+  path: 'transfers',
+  component: TransfersComponent
+}, {
+  path: 'statements',
+  component: StatementsComponent
+}, {
+  path: 'not-found',
   component: NotFoundComponent
+}, {
+  path: '',
+  pathMatch: 'full',
+  redirectTo: 'login'
+}, {
+  path: '**',
+  redirectTo: 'not-found'
 }];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { NotFoundComponent } from './components/shared/not-found/not-found.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { TransfersComponent } from './pages/transfers/transfers.component';
+import { StatementsComponent } from './pages/statements/statements.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    FooterComponent,
+    HeaderComponent,
+    TransfersComponent,
+    StatementsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
