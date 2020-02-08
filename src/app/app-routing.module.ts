f8d406411262f7ff2c2a5a48cbb0eb3ab44ae5d1
Avanade <Avanade@WIN-WBJAUUHC8C.corp.avanade.org>
2020-02-08 09:29:24 -0300

    tela de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+
+import { NotFoundComponent } from './components/shared/not-found/not-found.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'login',
+  component: LoginPageComponent
+},{
+  path: '**',
+  component: NotFoundComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { NotFoundComponent } from './components/shared/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
